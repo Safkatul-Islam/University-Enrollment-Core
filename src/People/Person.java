5c07058a1728243aa03d828cf1b7a9af7651b54c
Md Safkatul Islam <mrsafkatul.stu24@gmail.com>
2025-10-02 20:57:12 -0700

    Add an abstract person class for the project

---
+++ src/People/Person.java
@@ -0,0 +1,19 @@
+package People;
+
+public abstract class Person {
+    private int id;
+    private String name;
+
+    public Person(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() { return id; }
+
+    public String getName() { return name; }
+
+    public void introduce() {
+        System.out.print("I am " + name + " (ID - " + id + ")");
+    }
+}
